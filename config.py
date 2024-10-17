e5b0dd6eaee96f930044ef75f0157067846b3994
mrsontu <mrsontu.tn@gmail.com>
2024-10-17 11:03:28 +0700

    change confgi

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class BaseConfig:
     POSTGRES_USER="azAdmin" #TODO: Update value
     POSTGRES_PW="abcd1234_"   #TODO: Update value
     POSTGRES_DB="project3"   #TODO: Update value
-    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=azAdmin,pw=abcd1234_,url=project3.postgres.database.azure.com,db=project3)
-    SQLALCHEMY_DATABASE_URI = DB_URL
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(POSTGRES_USER,POSTGRES_PW,POSTGRES_URL,POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'QRAKxlm8DIusTVB13izNCiCQQCRsd2w2N+ASbOkzfCc='
     SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://project3.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=QRAKxlm8DIusTVB13izNCiCQQCRsd2w2N+ASbOkzfCc=' #TODO: Update value
